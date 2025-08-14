53e5a283210ef99f6f04fdac06d9ff9ffd9b2c6a
ztionbar <ztion1990@gmail.com>
2025-08-14 20:32:49 +0800

    修改vite.config.js內容及安裝gh-pages套件

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
+  // 開發中、產品路徑
+  base: mode === 'production' ? '/react-vite-test-one/' : '/' ,
   plugins: [react()],
-})
+}))
