0d473eb7af0a9b9594b5a3d5ed4cabf71aebd741
ztionbar <ztion1990@gmail.com>
2025-08-19 18:30:35 +0800

    新增boostratp modal 效果

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // 外部資源
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
+import { Modal } from 'bootstrap'
 // 內部資源
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,17 +9,63 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const modalRef = useRef(null)
+  const customModal = useRef(null)
 
   useEffect(() =>{
     (async () => {
       const res = await axios.get('https://randomuser.me/api/')
       console.log(res);
+      openModal();
+
+      setTimeout(() => {
+        closeModal()
+      }, 2000);
 
     })()
   }, [])
 
+  useEffect(() =>{
+    // console.log(modalRef.current);
+    customModal.current = new Modal(modalRef.current);
+    // customModal.current.show()
+  }, [])
+
+  const openModal = () => {
+    customModal.current.show()
+  }
+
+  const closeModal = () => {
+    customModal.current.hide()
+  }
+
   return (
     <>
+      
+    <button type="button" className="btn btn-primary"
+      onClick={() => openModal()}>
+      Launch demo modal
+    </button>
+
+
+    <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            ...
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
